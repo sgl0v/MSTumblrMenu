bd8389210bf91f0a87a7dc47546f13d94aff8041
Maksym Shcheglov <maxscheglov@gmail.com>
2015-07-09 23:17:24 +0200

    Created view controller for the tumblr menu.

---
+++ MSTumblrMenu/MSTumblrMenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MSTumblrMenuViewController.swift
+//  MSTumblrMenu
+//
+//  Created by Maksym Shcheglov on 09/07/15.
+//  Copyright (c) 2015 Maksym Shcheglov. All rights reserved.
+//
+
+import UIKit
+
+class MSTumblrMenuViewController: NSObject {
+   
+}
