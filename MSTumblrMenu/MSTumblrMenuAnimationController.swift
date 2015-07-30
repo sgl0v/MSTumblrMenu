1afc43d3ee1e0dd55c4f0f4be946a5e390be435a
Maksym Shcheglov <maxscheglov@gmail.com>
2015-07-30 23:06:11 +0200

    Removed leftovers from the menu animation controller.

--- MSTumblrMenu/MSTumblrMenuAnimationController.swift
+++ MSTumblrMenu/MSTumblrMenuAnimationController.swift
@@ -11,77 +11,36 @@ import UIKit
 class MSTumblrMenuAnimationController: NSObject {
 
     private let presenting: Bool
-    private var minOffset: CGFloat {
-        return CGRectGetHeight(UIScreen.mainScreen().bounds) / 3
-    }
-    private var maxOffset: CGFloat {
-        return self.minOffset * 2
-    }
 
     init(presenting: Bool) {
         self.presenting = presenting
         super.init()
     }
-
-    private func prepareForPresentingViewController(menuViewController: MSTumblrMenuViewController) {
-        menuViewController.view.alpha = 0.0
-
-//        menuViewController.menuItem1.transform = CGAffineTransformMakeTranslation(0.0, maxOffset)
-//        menuViewController.menuItem2.transform = CGAffineTransformMakeTranslation(0.0, minOffset)
-//        menuViewController.menuItem3.transform = CGAffineTransformMakeTranslation(0.0, maxOffset)
-    }
-
-    private func presentViewController(menuViewController: MSTumblrMenuViewController) {
-        menuViewController.view.alpha = 0.5
-        menuViewController.addItems()
-        
-//        menuViewController.addItems()
-
-//        menuViewController.menuItem1.transform = CGAffineTransformIdentity
-//        menuViewController.menuItem2.transform = CGAffineTransformIdentity
-//        menuViewController.menuItem3.transform = CGAffineTransformIdentity
-    }
-
-    private func dismissViewController(menuViewController: MSTumblrMenuViewController) {
-        menuViewController.view.alpha = 0.0
-
-//        menuViewController.menuItem1.transform = CGAffineTransformMakeTranslation(0.0, -minOffset)
-//        menuViewController.menuItem2.transform = CGAffineTransformMakeTranslation(0.0, -maxOffset)
-//        menuViewController.menuItem3.transform = CGAffineTransformMakeTranslation(0.0, -minOffset)
-    }
-
 }
 
 extension MSTumblrMenuAnimationController: UIViewControllerAnimatedTransitioning {
 
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
-        return 0.6
+        return 1
     }
 
-    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-        let container = transitionContext.containerView()
-        let controllers : (from: UIViewController, to: UIViewController) = (transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!, transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!)
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning)  {
+        let presentedControllerView = self.presenting ? transitionContext.viewForKey(UITransitionContextToViewKey)! : transitionContext.viewForKey(UITransitionContextFromViewKey)!
+        let containerView = transitionContext.containerView()
 
-        let menuViewController = self.presenting ? controllers.to as! MSTumblrMenuViewController : controllers.from as! MSTumblrMenuViewController
-        let bottomViewController = self.presenting ? controllers.from : controllers.to
-
-        container!.addSubview(bottomViewController.view)
-        container!.addSubview(menuViewController.view)
         if self.presenting {
-            self.prepareForPresentingViewController(menuViewController)
+            presentedControllerView.alpha = 0
+            containerView!.addSubview(presentedControllerView)
+            let menuViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) as! MSTumblrMenuViewController
+            menuViewController.addItems()
         }
 
-        let duration = self.transitionDuration(transitionContext)
-
-        UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: [], animations: { () -> Void in
-            if self.presenting {
-                self.presentViewController(menuViewController)
-            } else {
-                self.dismissViewController(menuViewController)
-            }
-            }) { finished in
-                transitionContext.completeTransition(true)
-        }
+        UIView.animateWithDuration(self.transitionDuration(transitionContext), delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.0, options: .AllowUserInteraction, animations: {
+            presentedControllerView.alpha = self.presenting ? 0.5 : 0
+            }, completion: {(completed: Bool) -> Void in
+                transitionContext.completeTransition(completed)
+        })
     }
+
 }
 
