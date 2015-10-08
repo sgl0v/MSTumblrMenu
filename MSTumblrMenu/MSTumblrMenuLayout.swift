035832564d2feffd030ea1e3ca1133f6eb0d1f0f
Maksym Shcheglov <maxscheglov@gmail.com>
2015-10-08 22:21:00 +0100

    Refactored the menu layout class.

--- MSTumblrMenu/MSTumblrMenuLayout.swift
+++ MSTumblrMenu/MSTumblrMenuLayout.swift
@@ -12,36 +12,16 @@ class MSTumblrMenuLayout: UICollectionViewLayout {
 
     var cellSize = CGSizeMake(100, 120)
     let cellSpace: CGFloat = 20.0
-    let rowsCount = 3
-    private var indexPathsToAnimate : Array<NSIndexPath>!
-    var cachedAttributes = [NSIndexPath: MSTumblrMenuLayoutAttributes]()
+    private var indexPathsToAnimate = [NSIndexPath]()
+    private var layoutAttributes: [NSIndexPath: UICollectionViewLayoutAttributes]!
 
     override func prepareLayout() {
         super.prepareLayout()
-        self.cachedAttributes = [NSIndexPath: MSTumblrMenuLayoutAttributes]()
-        let height = self.collectionViewContentSize().height
-        let width = self.collectionViewContentSize().width
-        let numberOfSections = self.collectionView!.numberOfSections()
-        let minY = (height - CGFloat(numberOfSections) * (self.cellSize.height + self.cellSpace) + self.cellSpace) / 2
-        for section in 0..<numberOfSections {
-            let numberOfItems = self.collectionView!.numberOfItemsInSection(section)
-            let minX = (width - CGFloat(rowsCount) * (self.cellSize.width + self.cellSpace) + self.cellSpace) / 2
-            for row in 0..<numberOfItems {
-                let indexPath = NSIndexPath(forRow: row, inSection: section)
-                let attributes = MSTumblrMenuLayoutAttributes(forCellWithIndexPath: indexPath)
-                attributes.size = self.cellSize
-                attributes.center = CGPointMake(minX + CGFloat(indexPath.row) * (self.cellSize.width + self.cellSpace) + self.cellSize.width / 2, minY + CGFloat(indexPath.section) * (self.cellSize.height + self.cellSpace) + self.cellSize.height / 2)
-                self.cachedAttributes[indexPath] = attributes
-            }
-        }
-    }
-
-    override class func layoutAttributesClass() -> AnyClass {
-        return MSTumblrMenuLayoutAttributes.self
+        self.layoutAttributes = self.generateLayoutAttributes()
     }
 
     override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        return self.cachedAttributes.map({ (_, attributes) -> UICollectionViewLayoutAttributes in
+        return self.layoutAttributes.map({ (_, attributes) -> UICollectionViewLayoutAttributes in
             return attributes
         }).filter { attributes -> Bool in
             return CGRectIntersectsRect(attributes.frame, rect)
@@ -49,7 +29,7 @@ class MSTumblrMenuLayout: UICollectionViewLayout {
     }
 
     override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-        return self.cachedAttributes[indexPath]
+        return self.layoutAttributes[indexPath]
     }
 
     override func collectionViewContentSize() -> CGSize {
@@ -60,49 +40,30 @@ class MSTumblrMenuLayout: UICollectionViewLayout {
         }
     }
 
-//    override func initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-//        if self.indexPathsToAnimate.indexOf(itemIndexPath) == nil {
-//            return super.initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath)
-//        }
-//        let offset = self.collectionViewContentSize().height
-//        let attributes = UICollectionViewLayoutAttributes(forCellWithIndexPath: itemIndexPath)
-////        attributes.alpha = 1.0
-//        attributes.size = self.cellSize
-//        attributes.center = CGPointMake(self.cachedAttributes[itemIndexPath]!.center.x,  offset + self.cachedAttributes[itemIndexPath]!.center.y)
-//        return attributes
-//    }
-//
-//
-//    override func finalLayoutAttributesForDisappearingItemAtIndexPath(itemIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-//        if self.indexPathsToAnimate.indexOf(itemIndexPath) == nil {
-//            return super.finalLayoutAttributesForDisappearingItemAtIndexPath(itemIndexPath)
-//        }
-//        let attributes = UICollectionViewLayoutAttributes(forCellWithIndexPath: itemIndexPath)
-////        attributes.alpha = 1.0
-//        attributes.size = self.cellSize
-//        attributes.center = CGPointMake(CGFloat(itemIndexPath.row) * (cellSize.width + cellSpace) + cellSize.width / 2,  -cellSize.height / 2)
-//        return attributes
-//    }
-
-    override func prepareForCollectionViewUpdates(updateItems: [UICollectionViewUpdateItem]) {
-        super.prepareForCollectionViewUpdates(updateItems)
-        self.indexPathsToAnimate = [NSIndexPath]()
-        for updateItem in updateItems {
-            if (updateItem.updateAction == .Insert) {
-                self.indexPathsToAnimate.append((updateItem as UICollectionViewUpdateItem).indexPathAfterUpdate)
-            } else if (updateItem.updateAction == .Delete) {
-                self.indexPathsToAnimate.append((updateItem as UICollectionViewUpdateItem).indexPathBeforeUpdate)
-            }
-        }
-    }
-
-    override func finalizeCollectionViewUpdates() {
-        self.indexPathsToAnimate = [NSIndexPath]()
-    }
-
     override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
         return true
     }
 
     // MARK: Private
+
+    private func generateLayoutAttributes() -> [NSIndexPath: UICollectionViewLayoutAttributes] {
+        var layoutAttributes = [NSIndexPath: UICollectionViewLayoutAttributes]()
+        let height = self.collectionViewContentSize().height
+        let width = self.collectionViewContentSize().width
+        let numberOfSections = self.collectionView!.numberOfSections()
+        let minY = (height - CGFloat(numberOfSections) * (self.cellSize.height + self.cellSpace) + self.cellSpace) / 2
+        for section in 0..<numberOfSections {
+            let numberOfItems = self.collectionView!.numberOfItemsInSection(section)
+            let minX = (width - CGFloat(numberOfItems) * (self.cellSize.width + self.cellSpace) + self.cellSpace) / 2
+            for row in 0..<numberOfItems {
+                let indexPath = NSIndexPath(forRow: row, inSection: section)
+                let attributes = UICollectionViewLayoutAttributes(forCellWithIndexPath: indexPath)
+                attributes.size = self.cellSize
+                attributes.center = CGPointMake(minX + CGFloat(indexPath.row) * (self.cellSize.width + self.cellSpace) + self.cellSize.width / 2, minY + CGFloat(indexPath.section) * (self.cellSize.height + self.cellSpace) + self.cellSize.height / 2)
+                layoutAttributes[indexPath] = attributes
+            }
+        }
+        return layoutAttributes
+    }
+
 }
